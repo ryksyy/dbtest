f8722405eb47f0c73705ae53cadca3e517acaa98
Miko <miko.prykari@gmail.com>
2022-10-17 14:03:26 +0300

    init

---
+++ server/db.sql
@@ -0,0 +1,11 @@
+drop database if exists todo;
+create database todo;
+use todo;
+
+create table task (
+    id int primary key auto_increment,
+    description varchar(255) not null
+);
+
+insert into task (description) values('my test task');
+insert into task (description) values('next test');
